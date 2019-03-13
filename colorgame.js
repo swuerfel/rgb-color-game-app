3738f06f50006cb52ded980b71420c56224affa0
swuerfel <sebawue@gmail.com>
2019-03-13 08:34:57 +0100

    Added pick random color as picked color for winning the game

--- colorgame.js
+++ colorgame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,33 +25,24 @@ for(var i = 0; i < squares.length; i++){
 
     //compare color to pickedColor
     if(clickedColor === pickedColor){
-      alert("correct!");
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
     }  else {
       this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
 }
 
+function changeColors(color) {
+  // loop through all squares
+  for(var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+}
 
-// var squares = document.querySelectorAll(".square");
-// var pickedColor = colors[3];
-// var colorDisplay = document.getElementById("colorDisplay");
-//
-// colorDisplay.textContent = pickedColor;
-//
-// for(var i = 0; i < squares.length; i++){
-// 	// add initial colors to squares
-// 	squares[i].style.background = colors[i];
-//
-// 	//add click listeners to squares
-// 	squares[i].addEventListener("click", function() {
-// 		//grab color of clicked squares
-// 		var clickedColor = this.style.background;
-// 		//compare color to pickedColor
-// 		if(clickedColor === pickedColor) {
-// 			alert("Correct!");
-// 		} else {
-// 			alert("WRONG!!!");
-// 		}
-// 	});
-// }
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random];
+}
