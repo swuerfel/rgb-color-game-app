24a61833729e67bc7702c350d40fc5f72e83f309
swuerfel <sebawue@gmail.com>
2019-03-13 10:40:10 +0100

    Filled colors array with random rgb colors

--- colorgame.js
+++ colorgame.js
@@ -1,16 +1,10 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -27,6 +21,7 @@ for(var i = 0; i < squares.length; i++){
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     }  else {
       this.style.background = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -45,4 +40,28 @@ function changeColors(color) {
 function pickColor(){
   var random = Math.floor(Math.random() * colors.length)
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  // make an array
+  var arr = []
+  // repeat num times
+  for (var i = 0; i < num; i++){
+    // get random color and push into arr
+    arr.push(randomColor());
+  }
+  // return array at the end
+  return arr;
+}
+
+function randomColor(){
+  //pick a red from 0 to 255
+  var r = Math.floor(Math.random() * 256);
+  // pick a green from 0 to 255
+  var g = Math.floor(Math.random() * 256);
+  //  pick a blue from 0 to 255
+  var b = Math.floor(Math.random() * 256);
+
+  // Goal: get in format of "rgb(r. g. b)"
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
