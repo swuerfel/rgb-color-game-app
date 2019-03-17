6e204d6e1c363cee4702fdca00f0ba9179849ac9
swuerfel <sebawue@gmail.com>
2019-03-17 08:29:54 +0100

    Added logic and style of easy and hard button for color game

--- colorgame.js
+++ colorgame.js
@@ -1,3 +1,4 @@
+var numSquares = 6;
 var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
@@ -6,10 +7,42 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++){
+    if(colors[i]){
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+})
+
+hardBtn.addEventListener("click", function(){
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++){
+      squares[i].style.background = colors[i];
+      squares[i].style.display = "block";
+  }
+})
 
 resetButton.addEventListener("click", function(){
   // generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
 
   // pick  new random color from array
   pickedColor = pickColor();
